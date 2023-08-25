b59023a7cadcb04f8e5a511da292dc665cf6ca64
MrMarvel <seregakkk999@yandex.ru>
2023-08-25 17:00:40 +0300

    Trying to resolve dependencies

---
+++ app/src/main/java/ru/neuron/sportapp/MainActivity2.kt
@@ -0,0 +1,20 @@
+package ru.neuron.sportapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity2: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var startDestination = "login_screen"
+        setContent {
+            Text("")
+            val navController = rememberNavController()
+            val context = LocalContext.current
+        }
+    }
+}
