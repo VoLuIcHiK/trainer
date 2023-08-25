401e05753773c239ee2bf57f8a4124f7cf203f0b
MrMarvel <seregakkk999@yandex.ru>
2023-08-25 19:38:53 +0300

    Added styles "default" from examples

--- app/src/main/java/ru/neuron/sportapp/MainActivity.kt
+++ app/src/main/java/ru/neuron/sportapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.navigation.compose.rememberNavController
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var startDestination = "login_screen"
+        var startDestination = "home"
         setContent {
             Text("")
             val navController = rememberNavController()
---
+++ app/src/main/java/ru/neuron/sportapp/ui/SportTheme.kt
@@ -0,0 +1,40 @@
+package ru.neuron.sportapp.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+val crane_caption = Color.DarkGray
+val crane_divider_color = Color.LightGray
+private val craneRed = Color(0xFFE30425)
+private val craneWhite = Color.White
+private val cranePurple700 = Color(0xFF720D5D)
+private val cranePurple800 = Color(0xFF5D1049)
+private val cranePurple900 = Color(0xFF4E0D3A)
+
+val craneColors = lightColorScheme(
+    primary = cranePurple800,
+    secondary = craneRed,
+    surface = cranePurple900,
+    onSurface = craneWhite,
+    onPrimary = cranePurple700
+)
+
+val BottomSheetShape = RoundedCornerShape(
+    topStart = 20.dp,
+    topEnd = 20.dp,
+    bottomStart = 0.dp,
+    bottomEnd = 0.dp
+)
+
+@Composable
+fun CraneTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = craneColors,
+        typography = craneTypography) {
+        content()
+    }
+}
