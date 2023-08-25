3af8349d113cda7e40817a5294bcea2c14a7da7f
borntowarn <kozlovmihail02@mail.ru>
2023-08-26 01:21:06 +0300

    Added opencv

--- app/src/main/java/ru/neuron/sportapp/home/HomeViewModel.kt
+++ app/src/main/java/ru/neuron/sportapp/home/HomeViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import org.bytedeco.opencv.opencv_core.Mat
+import org.bytedeco.opencv.opencv_videoio.VideoCapture
 
 class HomeViewModel: ViewModel() {
     fun onVideoSelected(videoSelectedUri: Uri) {
