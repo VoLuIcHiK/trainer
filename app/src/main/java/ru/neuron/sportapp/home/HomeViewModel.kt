2d6371e608f1b38df024d16491fce22a7b194da3
MrMarvel <seregakkk999@yandex.ru>
2023-08-26 01:27:38 +0300

    Change file input to inputstream

--- app/src/main/java/ru/neuron/sportapp/home/HomeViewModel.kt
+++ app/src/main/java/ru/neuron/sportapp/home/HomeViewModel.kt
@@ -4,10 +4,11 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import java.io.InputStream
 import java.time.LocalDate
 
 class HomeViewModel: ViewModel() {
-    fun onVideoSelected(videoSelectedUri: Uri) {
+    fun onVideoSelected(videoSelected: InputStream) {
         viewModelScope.launch {
 
         }
--- app/src/main/java/ru/neuron/sportapp/home/SportHome.kt
+++ app/src/main/java/ru/neuron/sportapp/home/SportHome.kt
@@ -5,16 +5,19 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import org.jetbrains.kotlinx.dl.example.app.R
 
 @Composable
 fun SportHome(homeViewModel: HomeViewModel) {
+    val context = LocalContext.current
     val pickFileLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.GetContent()
     ) { videoUri ->
         if (videoUri != null) {
-            homeViewModel.onVideoSelected(videoUri)
+            context.contentResolver.openInputStream(videoUri)
+                ?.let { homeViewModel.onVideoSelected(it) }
         }
     }
     Button(onClick = {
