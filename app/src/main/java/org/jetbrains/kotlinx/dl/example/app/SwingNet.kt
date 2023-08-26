5a00ef88b1ab445edd2f6462614aa4339a5b65c4
borntowarn <kozlovmihail02@mail.ru>
2023-08-27 02:35:35 +0300

    Added VideoProcessor

--- app/src/main/java/org/jetbrains/kotlinx/dl/example/app/SwingNet.kt
+++ app/src/main/java/org/jetbrains/kotlinx/dl/example/app/SwingNet.kt
@@ -30,7 +30,9 @@ class SwingNet(
 //        byteBuffer.order(ByteOrder.nativeOrder())
         buffer = byteBuffer.asFloatBuffer()
         for (i in data){
-            buffer.put(i) /*from  w  w  w  . j  av a 2  s .c o  m*/
+            for (j in 0 until 3 step 1) {
+                buffer.put(i.slice(IntRange(j * 25600, (j + 1) * 25600 - 1)).toFloatArray())
+            }/*from  w  w  w  . j  av a 2  s .c o  m*/
         }
         buffer.position(0)
         return buffer
@@ -39,21 +41,46 @@ class SwingNet(
     fun directFloatBufferFromFloatArray1(data: FloatArray): FloatBuffer? {
         var buffer: FloatBuffer? = null
         val byteBuffer = ByteBuffer.allocateDirect(data.size * 4)
-//        byteBuffer.order(ByteOrder.nativeOrder())
+        byteBuffer.order(ByteOrder.BIG_ENDIAN)
         buffer = byteBuffer.asFloatBuffer()
         buffer.put(data) /*from  w  w  w  . j  av a 2  s .c o  m*/
         buffer.position(0)
         return buffer
     }
 
+    private fun getOutputImage(output: FloatBuffer): Bitmap {
+        output.rewind() // Rewind the output buffer after running.
+
+        val bitmap = Bitmap.createBitmap(160, 160, Bitmap.Config.ARGB_8888)
+        val pixels = IntArray(160 * 160) // Set your expected output's height and width
+        for (i in 0 until 160 * 160) {
+            val a = 0xFF
+            val r: Float = output.get() * 255.0f
+            val g: Float = output.get() * 255.0f
+            val b: Float = output.get() * 255.0f
+            pixels[i] = a shl 24 or (r.toInt() shl 16) or (g.toInt() shl 8) or b.toInt()
+        }
+        bitmap.setPixels(pixels, 0, 160, 0, 0, 160, 160)
+
+        return bitmap
+    }
+
     fun inference(sourceArray: ArrayList<FloatArray>): Array<FloatArray> {
-        val buf1 = directFloatBufferFromFloatArray(arrayListOf(floatArrayOf(1.1F, 2.2F), floatArrayOf(3.3F, 4.4F)))
-        val buf2 = directFloatBufferFromFloatArray1(floatArrayOf(1.1F, 2.2F, 3.3F, 4.4F))
+//        val buf1 = directFloatBufferFromFloatArray(arrayListOf(floatArrayOf(1.1F, 2.2F), floatArrayOf(3.3F, 4.4F)))
+//        val buf2 = directFloatBufferFromFloatArray1(floatArrayOf(1.1F, 2.2F, 3.3F, 4.4F))
 //        val arr2 = FloatArray(18432000 / 240 * 32)
 //        arr2.fill(0.0F)
+//        val newArr = FloatArray(sourceArray.size*3*160*160)
+//        for (i in sourceArray){
+//            newArr = conca
+//        }
+        val f_buff = directFloatBufferFromFloatArray1(sourceArray[0])
+        val f_img = getOutputImage(f_buff!!)
         val arr4 = directFloatBufferFromFloatArray(sourceArray)
         val arr3 = longArrayOf(1, sourceArray.size.toLong(), 3, 160, 160)
         val tensorFromArray = OnnxTensor.createTensor(env, arr4, arr3)
+//        val tensorFromArray1 = OnnxTensor.createTensor(env, FloatBuffer.wrap(sourceArray), arr3)
+
 //        val tensorFromArray1 = reshape(tensorFromArray, )
         var t1: OnnxTensor = tensorFromArray
         val inputs = mapOf<String, OnnxTensor>("input_1" to t1)
@@ -63,7 +90,7 @@ class SwingNet(
     }
 
     fun predict(bitmaps: ArrayList<Bitmap>): Array<FloatArray> {
-        var array = ArrayList<FloatArray>()
+        val array = ArrayList<FloatArray>()
         for (i in bitmaps){
             array.add(preprocessing.apply(i).first)
         }
@@ -93,7 +120,6 @@ class SwingNet(
             outputWidth = 160
         }
         .toFloatArray { layout = TensorLayout.NCHW }
-
 
 }
 
